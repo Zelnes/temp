6255338693fe1829103bae84f86b338249d3850a
Mehdi GHESH <mehdi.ghesh@sfr.com>
2018-10-02 23:56:34 +0200

    Clean of the code

--- bash_lib.sh
+++ bash_lib.sh
@@ -1,5 +1,57 @@
 #!/usr/bin/env bash
 
+parse_args_help() {
+cat <<-EOF
+  This bash function helps to parse cmd line arguments
+  It takes a list of understood option (short/long), for which their values can
+  be assigned to a specified variable.
+
+  Usage : parse_args "list of options understood" "original command line"
+
+  The list is space element separated. Each element must be an option, and can
+  be followed by an equal sign, "=", and a bash-like variable name.
+
+  If a variable is given, and the option associated is found, then the variable
+  is filled with the corresponding value.
+
+  Setting PARSE_VERBOSE to 'y' will cause the function to be verbose on what it
+  does
+
+  Extra functions :
+    - parse_args_get_opt_val <option> <list_of_options>
+      Outputs an option and the variable associated if presents
+    - parse_args_echo_vars var1 var2 ...
+      Outputs the variables given and their contents
+    - parse_args_test_func <cmd_line>
+      Simulates a call to a function that calls itself parse_args
+    - parse_args_call_test_func
+      Simulates the call to parse_args_test_func
+
+  Example :
+
+  my_func() {
+    parse_args "-s=varS --long=varL --alone" "\$@"
+  }
+
+  $ PARSE_VERBOSE=y my_func -s "something" --alone --long="like that" -e "oh yeah"
+  -s=varS --long=varL --alone
+  Short option '-s' => varS='something'
+  Long option '--alone'
+  Long option '--long' => varL='like that'
+  Value '-e' saved
+  Value 'oh yeah' saved
+
+  === Checking Global vars
+  PA_AO : --alone
+  PA_R  :  "-e" "oh yeah"
+
+  $ parse_args_echo_vars varS varL
+  === Checking vars...
+  varS = 'something'
+  varL = 'like that'
+EOF
+}
+
 parse_args_logger() {
   [ "${PARSE_VERBOSE}" != "y" ] || echo "$@"
 }
@@ -10,13 +62,10 @@ parse_args_get_opt_val() {
   sed -nr "s/.*-${1:1}(=?([^[:blank:]]*)).*/$1 \2/p" <<<"$2"
 }
 
-# PA_AO
-# PA_R
-# Arg 1 : option=var-name option var-name
-# Add possibility to give PA_R/PA_AO names
-# And if not present, use local losts variables
-# It may override otherwise
+# Arg 1 : option=var-name option
 # PARSE_VERBOSE=y -> verbose
+# PA_AO and PA_R : Output
+# TODO Add possibility to give PA_R/PA_AO names
 parse_args() {
   local options="$(printf "%s\n" "$1")"; shift
   local cmdl="$@"
@@ -57,7 +106,6 @@ parse_args() {
         done
       ;;
       --* )
-        # set -x
         read curO curV <<<$(parse_args_get_opt_val "$cur" "$options")
         if [[ -z "$curO" ]]; then
           cso="$1"
@@ -75,10 +123,8 @@ parse_args() {
             shift $toshift
           fi
         fi
-        # set +x
         ;;
       -* )
-      # set -x
         cso="-${cur:1:1}"
         [ ${#cur} -ne 2 ] && {
           keep=1
@@ -100,7 +146,6 @@ parse_args() {
             shift $toshift
           fi
         fi
-        # set +x
         ;;
       * )
         cso="$1"
@@ -117,19 +162,19 @@ parse_args() {
   parse_args_logger "PA_R  : ${PA_R}"
 }
 
-echo_vars() {
-  echo -e "\n=== Checking vars..."
+parse_args_echo_vars() {
+  echo "=== Checking vars..."
   for i in $@; do
     eval echo \"$i = \'\${$i}\'\"
   done
 }
 
-test_func() {
+parse_args_test_func() {
   echo " -- Calling for '$@'"
-  parse_args "-p=mon_option --autre=voyons une-autre --verbose=verbosity --alone -y" "$@"
-  echo_vars mon_option voyons verbosity
+  parse_args "-p=my_option --other=letssee --verbose=verbosity --alone -y" "$@"
+  parse_args_echo_vars my_option letssee verbosity
 }
 
-call_func() {
-   test_func -p porte -e quelquechose --verbose "Oui on peut" --autre="ca marche ca" --alone -y -e "Voila ce que je te dis"
+parse_args_call_test_func() {
+   parse_args_test_func -p door -e something --verbose "Yes we can" --other="it works also" --alone -y -e "This is what I say"
 }
