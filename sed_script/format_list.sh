ade8054027584acd1563342071ddd2cf206b9e7b
Mehdi GHESH <mehdi.ghesh@sfr.com>
2018-10-15 07:57:20 +0200

    My new little awk/sed program dependency

---
+++ sed_script/format_list.sh
@@ -0,0 +1,22 @@
+# https://misc.flogisoft.com/bash/tip_colors_and_formatting
+
+readonly C_BLUE=31
+readonly C_MAGENTA=35
+readonly C_GREEN=34
+readonly C_ORANGE=208
+readonly C_RED=196
+
+readonly C_MAKE=${C_GREEN}
+readonly C_DEFAULT=${C_GREEN}
+
+# Format definitions
+readonly F_NORMAL=0
+readonly F_BOLD=1
+readonly F_DIM=2
+readonly F_UNDER=4
+readonly F_BLINK=5
+readonly F_INV=7
+
+readonly F_DEFAULT=${F_NORMAL}
+
+readonly ADD_FILE=additional_values
